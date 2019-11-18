cdb9e8fc120223e23891e47765589d9918317328
vpec <victorpenasco1@gmail.com>
2019-11-18 23:20:14 +0100

    matching subtitle time lines

--- shifter3.py
+++ shifter3.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 fr = open(sys.argv[1],"rb")
 fw = open(sys.argv[1] + '.new',"wb+")
@@ -6,6 +7,9 @@ fw = open(sys.argv[1] + '.new',"wb+")
 if fr.mode == 'rb':
     lines = fr.readlines()
     for l in lines:
+        intervalPattern = re.compile(b"[0-9]+:[0-9]+:[0-9]+,[0-9]+ --> [0-9]+:[0-9]+:[0-9]+,[0-9]+")
+        if intervalPattern.match(l) :
+            print(l)
         fw.write(l)
 
 fr.close()
