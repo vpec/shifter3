007d1dd567db463c5ad2c5b98fe28da322b30530
vpec <741294@unizar.es>
2019-12-01 01:12:27 +0100

    Refactor all code (now using datetime module)

--- shifter3.py
+++ shifter3.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import datetime
 
 # Arguments:
 #   argv[1] -> input str file
@@ -7,58 +8,51 @@ import re
 
 fr = open(sys.argv[1],"rb")
 fw = open(sys.argv[1] + '.new',"wb+")
+# Get shift parameter
 shift = float(sys.argv[2])
 
 if fr.mode == 'rb':
+    # Get lines from file
     lines = fr.readlines()
+    # Create regex pattern for recognizing timestamp lines
     intervalPattern = re.compile(b"[0-9]+:[0-9]+:[0-9]+,[0-9]+ --> [0-9]+:[0-9]+:[0-9]+,[0-9]+")
     for l in lines:
         if intervalPattern.match(l) :
-            # print binary stream
-            print(l)
-            # Transform binary stream to string
-            lstr = str(l, 'utf-8')
-            # Create a list with all numbers in lstr
-            numberslist = re.findall(r'\d+', lstr)
-            # Insert dots in list to make conversion to float easier
-            numberslist.insert(3, '.')
-            numberslist.insert(8, '.')
-            # Join each numbersList into a single string
-            time1str = ''.join(numberslist[:5])
-            time2str = ''.join(numberslist[5:])
-            # Convert string to float
-            time1f = float(time1str)
-            time2f = float(time2str)
-            # Print strings
-            print(time1str)
-            print(time2str)
-            # Print floats
-            print(time1f)
-            print(time2f)
-            # Print floats + shift (rounded at miliseconds)
-            new_time1f = "{0:.3f}".format(time1f + shift)
-            new_time2f = "{0:.3f}".format(time2f + shift)
-            print(new_time1f)
-            print(new_time2f)
-            # Get the new time string (adding zeros to the left if necessary)
-            print(len(time1str))
-            print(len(str(new_time1f)))
-            new_time1str = '0' * (len(time1str) - len(str(new_time1f))) + str(new_time1f)
-            new_time2str = '0' * (len(time2str) - len(str(new_time2f))) + str(new_time2f)
-            print(new_time1str)
-            print(new_time2str)
-            o = len(time1str) - 10 # Necessary if hours > 99
-            new_time1str = new_time1str[:(2 + o)] + ':' +  new_time1str[(2 + o):(4 + o)] + ':' +  new_time1str[(4 + o):]
-            new_time1str = new_time1str.replace('.', ',')
-            new_time2str = new_time2str[:(2 + o)] + ':' +  new_time2str[(2 + o):(4 + o)] + ':' +  new_time2str[(4 + o):]
-            new_time2str = new_time2str.replace('.', ',')
-            print(new_time1str)
-            print(new_time2str)
+            # Create a list with all numbers in line
+            numberslist = re.findall(r'\d+', str(l, 'utf-8'))
+            # Divide list in two
+            list1 = numberslist[:4]
+            list2 = numberslist[4:]
+            # Get seconds from string
+            secs1 = float(list1[0])*3600 + float(list1[1])*60 + float(list1[2]) + float(list1[3]) / 1000.0
+            secs2 = float(list2[0])*3600 + float(list2[1])*60 + float(list2[2]) + float(list2[3]) / 1000.0
+            # Apply shift
+            new_secs1 = secs1 + shift
+            new_secs2 = secs2 + shift
+            # Check that timestamp is never negative
+            if new_secs1 < 0.0 :
+                new_secs1 = 0.0
+            if new_secs2 < 0.0 :
+                new_secs2 = 0.0
+            # Get integer part from seconds float
+            int_new_secs1 = int(new_secs1)
+            int_new_secs2 = int(new_secs2)
+            # Compose new time strings
+            new_time1str = str(datetime.timedelta(seconds=int_new_secs1)) + "," + "{0:.3f}".format(new_secs1 - int_new_secs1)[2:]
+            new_time2str = str(datetime.timedelta(seconds=int_new_secs2)) + "," + "{0:.3f}".format(new_secs2 - int_new_secs2)[2:]
+            # Add another left zero to hours if necessary
+            if len(new_time1str) < 12 :
+                new_time1str = "0" + new_time1str
+            if len(new_time2str) < 12 :
+                new_time2str = "0" + new_time2str
+            # New full string
             new_str = new_time1str + ' --> ' + new_time2str + '\r\n'
-            print(bytes(new_str, 'utf-8'))
+            # Write new full string to file
             fw.write(bytes(new_str, 'utf-8'))
         else :
+            # Write line to file
             fw.write(l)
-
+# Close files
 fr.close()
 fw.close()
+print("Shifted " + str(shift) + " seconds")
